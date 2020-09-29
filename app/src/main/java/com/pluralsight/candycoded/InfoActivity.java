59457d71b0e7ebe457e0a01b712714e9623cfadb
Emanuel31 <emanuelza1988@gmail.com>
2020-09-29 12:38:55 +0100

    Module completed

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -25,10 +25,7 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
-
+     // Launch the Google Maps Activity
     public void createMapIntent(View view) {
         // Creates a URI for a static address
         Uri address = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
@@ -46,4 +43,12 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        Uri uri = Uri.parse("tel:0123456789");
+        intent.setData(uri);
+
+        startActivity(intent);
+    }
 }
